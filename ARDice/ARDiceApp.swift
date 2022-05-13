08f93149a753da3812808daddfc05875318163d6
Dara Vasconcelos <CONS751@modal.net.br>
2022-05-13 09:58:55 -0300

    Initial Commit

---
+++ ARDice/ARDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARDiceApp.swift
+//  ARDice
+//
+//  Created by Ilia on 13/05/22.
+//
+
+import SwiftUI
+
+@main
+struct ARDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
